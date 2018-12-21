0cb06979ef7c79c7e475b0a30c3696237becb5e1
Grant Watson <grant@triumphdesigns.org>
2018-12-21 09:06:28 -0500

    IEnumerable for Roles Created

--- Grit.Web/Entities/Employee.cs
+++ Grit.Web/Entities/Employee.cs
@@ -5,7 +5,17 @@ using System.ComponentModel.DataAnnotations;
 namespace Grit.Web.Entities
 {     public class Employee     {         [Key]         public int EmpID { get; set; }          [Display(Name = "FirstName")]         public string FirstName { get; set; }          [Display(Name = "LastName")]         public string LastName { get; set; }          [Display(Name = "Name")]         public string FullName         {             get { return (FirstName + " " + LastName); }         }          public string Email { get; set; }         public string Phone { get; set; }         public string Notes { get; set; }
 
-        public class ProjectList
+        public IEnumerable<Role> Roles { get; set; }
+        public IEnumerable<Project> Projects { get; set; }
+       
+
+        public class Role
         {
-            public List<Project> Projects { get; set; }
+            [Display(Name = "Lead Developer")]
+            public Role LeadDev { get; set; }
+            [Display(Name = "Developer")]
+            public Role Dev { get; set; }
+            public Role Sales { get; set; }
+            public Role Admin { get; set; }
+            public Role Manager { get; set; }
         }     } }
