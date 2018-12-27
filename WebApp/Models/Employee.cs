e730fdc2fab5f04d016c21f501948446a4766170
Grant Watson <grant@triumphdesigns.org>
2018-12-27 12:05:32 -0500

    Making those money moves

--- WebApp/Models/Employee.cs
+++ WebApp/Models/Employee.cs
@@ -6,9 +6,10 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebApp.Models
 {
-    public class Employee     {         [Key]         public int EmpID { get; set; }          [Display(Name = "FirstName")]         public string FirstName { get; set; }          [Display(Name = "LastName")]         public string LastName { get; set; }          [Display(Name = "Name")]         public string FullName         {             get { return (FirstName + " " + LastName); }         }          public string Email { get; set; }         public string Phone { get; set; }         public string Notes { get; set; }
+    public class Employee     {         [Key]         public int EmpID { get; set; }          [Display(Name = "First Name")]         public string FirstName { get; set; }          [Display(Name = "Last Name")]         public string LastName { get; set; }          [Display(Name = "Name")]         public string FullName         {             get { return (FirstName + " " + LastName); }         }          public string Email { get; set; }         public string Phone { get; set; }         public string Notes { get; set; }
         public IEnumerable<Project> Projects { get; set; }
 
+        [Display(Name = "Roles")]
         public Roles EnumRoles { get; set; }
         public enum Roles
         {
