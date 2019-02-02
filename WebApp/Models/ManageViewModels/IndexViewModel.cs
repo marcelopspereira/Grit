2ac9861f9c0465639995cea353178b164832cb47
Grant Watson <grant@triumphdesigns.org>
2019-02-02 16:37:48 -0500

    User Manager VM addition

---
+++ WebApp/Models/ManageViewModels/IndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.ManageViewModels
+{
+    public class IndexViewModel
+    {
+        public string Username { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
