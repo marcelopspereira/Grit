820192bde926d2ef810933a0102a1852ee9d223d
Grant Watson <grant@triumphdesigns.org>
2019-02-02 13:48:24 -0500

    Combining #1

---
+++ WebApp/Models/AccountViewModel/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.AccountViewModel
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
