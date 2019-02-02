45e4571784977607fd094bc2f31bef244c2394dd
Grant Watson <grant@triumphdesigns.org>
2019-02-02 13:51:50 -0500

    Combining #2

---
+++ WebApp/Models/AccountViewModel/LoginWithRecoveryCodeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models.AccountViewModel
+{
+    public class LoginWithRecoveryCodeViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Recovery Code")]
+        public string RecoveryCode { get; set; }
+    }
+}
